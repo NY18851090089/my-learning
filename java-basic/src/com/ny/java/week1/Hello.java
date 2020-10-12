df28dbd1551d49b80c188859ded54bb5c33afa3b
NY18851090089 <71701237+NY18851090089@users.noreply.github.com>
2020-10-12 19:27:35 +0800

    10.12

---
+++ java-basic/src/com/ny/java/week1/Hello.java
@@ -0,0 +1,13 @@
+package com.ny.java.week2;
+
+/**
+ * @ClassName Hello
+ * @Description TODO
+ * @Author ny
+ * @Date 2020/10/12
+ **/
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
